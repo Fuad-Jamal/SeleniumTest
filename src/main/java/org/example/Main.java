ffa9be7cf3ec2ece0c3355aa7c5088240cf94a79
Fuad Jamal <jamalfuad34@gmail.com>
2025-12-01 22:11:12 +0200

    applying feedback for main file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 public class Main {
-   public static void main() {
+   public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
